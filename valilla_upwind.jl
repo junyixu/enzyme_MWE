e40c561e2d7b7cad7db406c5d15cb863938fa0c2
Junyi Xu <junyixu0@gmail.com>
2024-06-24 20:21:49 +0800

    fix: deepcopy every cache to get the right result
    cache_shadows[1] !=== cache_shadows[2]

--- valilla_upwind.jl
+++ valilla_upwind.jl
@@ -24,15 +24,14 @@ function upwind!(du::Vector, u::Vector, cache::Cache)
     du[1] = - C * (cache.numerical_flux[1] - cache.numerical_flux[end])
 end
 
-function jacobian_ad_forward_enzyme_cache_upwind_wrong(x::AbstractVector)
+function jacobian_ad_forward_enzyme_cache_upwind_right(x::AbstractVector)
     u_ode = zeros(length(x))
     du_ode = zeros(length(x))
     cache = Cache(1.0, zeros(length(x)))
 
     dy = Tuple(zeros(size(du_ode)) for _ in 1:length(u_ode))
     dx = Enzyme.onehot(u_ode)
-    cache_shadow=Cache(1.0, zeros(length(x)))
-    cache_shadows =Tuple(cache_shadow for i=1:length(x))
+    cache_shadows =Tuple(Cache(1.0, zeros(length(x))) for i=1:length(x))
 
     # cache is passed to upwind!
     Enzyme.autodiff(Enzyme.Forward, upwind!, Enzyme.BatchDuplicated(du_ode, dy), Enzyme.BatchDuplicated(u_ode, dx), Enzyme.BatchDuplicated(cache, cache_shadows))
@@ -40,5 +39,5 @@ function jacobian_ad_forward_enzyme_cache_upwind_wrong(x::AbstractVector)
 end
 
 x = -1.0:0.01:1.0
-jacobian_ad_forward_enzyme_cache_upwind_wrong(x)
+jacobian_ad_forward_enzyme_cache_upwind_right(x)
 
