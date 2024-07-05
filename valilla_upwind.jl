71ce1787e894e568baab3598711cc8735e7db3d5
Junyi Xu <junyixu0@gmail.com>
2024-07-05 17:17:38 +0800

    add: reverse mode AD

--- valilla_upwind.jl
+++ valilla_upwind.jl
@@ -16,7 +16,7 @@ struct Cache{T}
 end
 
 
-function upwind!(du::Vector, u::Vector, cache::Cache)
+function upwind!(du::Vector, u::Vector, cache)
     cache.numerical_flux .= u .* cache.v
 	for i = 2:length(u)
         du[i] = - C * (cache.numerical_flux[i] - cache.numerical_flux[i-1])  # Q_j^{n+1} = Q_j^n - Δt/Δx * ( F_{j+1/2}^n - F_{j-1/2}^n )
@@ -24,14 +24,24 @@ function upwind!(du::Vector, u::Vector, cache::Cache)
     du[1] = - C * (cache.numerical_flux[1] - cache.numerical_flux[end])
 end
 
+function upwind!(du::Vector, u::Vector, v, numerical_flux)
+    numerical_flux .= u * v
+	for i = 2:length(u)
+        du[i] = - C * (numerical_flux[i] - numerical_flux[i-1])  # Q_j^{n+1} = Q_j^n - Δt/Δx * ( F_{j+1/2}^n - F_{j-1/2}^n )
+	end
+    du[1] = - C * (numerical_flux[1] - numerical_flux[end])
+    return nothing # important for reverse mode AD
+end
+
+
 function jacobian_ad_forward_enzyme_cache_upwind_right(x::AbstractVector)
     u_ode = zeros(length(x))
     du_ode = zeros(length(x))
-    cache = Cache(1.0, zeros(length(x)))
+    cache = (;v=1.0, numerical_flux=zeros(length(x)))
 
     dy = Tuple(zeros(size(du_ode)) for _ in 1:length(u_ode))
     dx = Enzyme.onehot(u_ode)
-    cache_shadows =Tuple(Cache(1.0, zeros(length(x))) for i=1:length(x))
+    cache_shadows =Tuple((;v=1.0, numerical_flux=zeros(length(x))) for i=1:length(x))
 
     # cache is passed to upwind!
     Enzyme.autodiff(Enzyme.Forward, upwind!, Enzyme.BatchDuplicated(du_ode, dy), Enzyme.BatchDuplicated(u_ode, dx), Enzyme.BatchDuplicated(cache, cache_shadows))
@@ -47,21 +57,47 @@ x = -1.0:0.01:1.0
 function gradients_ad_forward_enzyme_cache_upwind(x::AbstractVector)
     u_ode = zeros(length(x))
     du_ode = zeros(length(x))
-    cache = Cache(1.0, zeros(length(x)))
+    numerical_flux= zeros(length(x))
 
     dy = zeros(size(du_ode))
     dx = zeros(size(u_ode))
-    cache_shadow = Cache(1.0, zeros(length(x)))
+    numerical_flux_shadow= zeros(length(x))
     dys = zeros(length(du_ode), length(du_ode))
 
     for i in 1:length(x)
-        dx .= 0.0
         dx[i] = 1.0
         # cache is passed to upwind!
-        Enzyme.autodiff(Enzyme.Forward, upwind!, Enzyme.Duplicated(du_ode, dy), Enzyme.Duplicated(u_ode, dx), Enzyme.Duplicated(cache, cache_shadow))
+        Enzyme.autodiff(Enzyme.Forward, upwind!, Enzyme.Duplicated(du_ode, dy), Enzyme.Duplicated(u_ode, dx), Const(1.0),Enzyme.Duplicated(numerical_flux, numerical_flux_shadow))
         dys[:, i] .= dy
+        dx[i] = 0.0
     end
     return dys
 end
+# %%
 
-@time gradients_ad_forward_enzyme_cache_upwind(x);
+function gradients_ad_reverse_enzyme_cache_upwind(x::AbstractVector)
+    u_ode = zeros(length(x))
+    du_ode = zeros(length(x))
+    numerical_flux=zeros(length(x))
+
+    dy = zeros(size(du_ode))
+    dx = zeros(size(u_ode))
+    dxs = zeros(length(du_ode), length(du_ode))
+    numerical_flux_shadow=zeros(length(x))
+
+    for i in 1:length(x)
+        dy[i] = 1.0
+        # cache is passed to upwind!
+        Enzyme.autodiff(Enzyme.Reverse, upwind!, Enzyme.Duplicated(du_ode, dy), Enzyme.Duplicated(u_ode, dx), Const(1.0),Enzyme.Duplicated(numerical_flux, numerical_flux_shadow))
+        dxs[i, :] .= dx
+        dx .= 0
+    end
+    return dxs
+end
+# %%
+# Const is default?
+
+x = -1.0:0.01:1.0
+@time J1=gradients_ad_forward_enzyme_cache_upwind(x)
+@time J2=gradients_ad_reverse_enzyme_cache_upwind(x)
+J1 == J2
